cb0b9003972f0150d2a8f0a694131b5800cab7e5
v.chebotnikov <v.chebotnikov@withsreda.com>
2025-04-06 17:53:19 +0300

    "delete old visitors"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-//        $middleware->append(\App\Http\Middleware\VisitorMiddleware::class);
 //        $middleware->append(\App\Http\Middleware\LocaleMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
